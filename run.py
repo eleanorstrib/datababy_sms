198a3b6e24c54a9933ad4fc0effcd93313f2276b
eleanorstrib <eleanor@sloan.mit.edu>
2015-11-02 22:02:17 -0800

    further testing

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ def hello():
 	if incoming_message is not None:
 		incoming_message = incoming_message.lower()
 	events = ['bottle', 'breast start', 'breast end', 'wet diaper', 'soiled diaper', 'sleep start', 'sleep end']
- 	help_message = "Text one of the following phrases to record your baby's activity:", events
+ 	help_message = ("Text one of the following phrases to record your baby's activity:", events)
 	
 	if incoming_message in events:
 		resp.message("We've recorded a %s for your baby." % (incoming_message))
